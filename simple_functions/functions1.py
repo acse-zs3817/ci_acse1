5b090a7b68d5075df8b9509ff41e7b40c64aaee4
acse-zs3817 <72441189+acse-zs3817@users.noreply.github.com>
2020-10-22 02:20:17 +0300

    added dummy sin function

--- simple_functions/functions1.py
+++ simple_functions/functions1.py
@@ -1,6 +1,6 @@
 from functools import lru_cache
 
-__all__ = ['my_sum', 'factorial']
+__all__ = ['my_sum', 'factorial', 'find_sin']
 
 
 def my_sum(iterable):
@@ -13,3 +13,6 @@ def my_sum(iterable):
 @lru_cache(maxsize=None)  # Note: -> @cache in python >= 3.9
 def factorial(n):
     return n * factorial(n-1) if n else 1
+
+def find_sin():
+    return None
